74ac93101b74bcbbf723ddb335f3032aa4c9f89e
Tanvir Kabir <tanvir@Tanvirs-MacBook-Pro.local>
2022-11-10 09:03:36 +0600

    sign up page added

--- src/components/pages/SignUp/SignUp.js
+++ src/components/pages/SignUp/SignUp.js
@@ -3,8 +3,50 @@ import { Link } from 'react-router-dom';
 
 const SignUp = () => {
     return (
-        <div>
-            
+        <div className="hero">
+            <div className="hero-content flex-col lg:flex-row-reverse">
+                <div className="text-center lg:text-left w-1/2">
+                    <h1 className="text-5xl font-bold">Login now!</h1>
+                    <p className="py-6">Please login to Mohammads Photograpy to book a service or leave a review. And enjoy the exciting features of Mohammads Photograpy</p>
+                </div>
+                <div className="card shadow-2xl w-1/2">
+                    <div className="card-body">
+                        <div className="form-control">
+                            <label className="label">
+                                <span className="label-text text-2xl">Name</span>
+                            </label>
+                            <input type="text" name='name' placeholder="Photo Url" className="input input-bordered" />
+                        </div>
+                        <div className="form-control">
+                            <label className="label">
+                                <span className="label-text text-2xl">Photo Url</span>
+                            </label>
+                            <input type="text" name='PhotoURL' placeholder="Photo Url" className="input input-bordered" />
+                        </div>
+                        <div className="form-control">
+                            <label className="label">
+                                <span className="label-text text-2xl">Email</span>
+                            </label>
+                            <input type="email" name='email' placeholder="email" className="input input-bordered" />
+                        </div>
+                        <div className="form-control">
+                            <label className="label">
+                                <span className="label-text text-2xl">Password</span>
+                            </label>
+                            <input type="password" name='password' placeholder="password" className="input input-bordered" />
+                            <label className="label">
+
+                            </label>
+                        </div>
+                        <div className="form-control mt-6">
+                            <button className="btn btn-primary">Sign Up</button>
+                        </div>
+                        <div>
+                            <p>Already have an account? <Link to='/login' className='btn btn-link'>Login</Link></p>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
